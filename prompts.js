0f64a8d5772e49e93c3180fbb1cdc1959e86a7b7
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-09 15:40:00 -0300

    finished prompt logic and glued all up together

--- prompts.js
+++ prompts.js
@@ -1,61 +1,64 @@
 import fetch from "node-fetch";
 import inquirer from "inquirer";
-//import { parseOptions } from "./saving.js";
+
+const promptForPokemon = async () => {
+  return await inquirer.prompt({
+    type: "input",
+    name: "pokemonName",
+    message: "Pokemon name:",
+  });
+};
+
+const promptForDownloadInfo = async () => {
+  return await inquirer.prompt({
+    type: "checkbox",
+    message: "Select info to download!",
+    name: "options",
+    choices: [
+      new inquirer.Separator(" -- Options -- "),
+      {
+        name: "Stats",
+      },
+      {
+        name: "Sprites",
+      },
+      {
+        name: "Artwork",
+      },
+    ],
+  });
+};
+
+const promptToContinue = async () => {
+  return await inquirer.prompt({
+    type: "confirm",
+    name: "continueAnotherDownload",
+    message: "Would you like to search for another pokémon?",
+  });
+};
 
 const fetchPokemon = async (pokemonName) => {
   const request = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
   );
   const response = await request.json();
-  console.log(response);
   return response;
 };
 
-const promptForPokemon = async () => {
-  const pokemonNamePrompt = [
-    {
-      type: "input",
-      name: "pokemonName",
-      message: "Pokemon name:",
-    },
-  ];
+const promptUser = async () => {
+  while (true) {
+    const pokemonName = await promptForPokemon();
+    console.log(pokemonName.pokemonName);
 
-  inquirer.prompt(pokemonNamePrompt).then((answers) => {
-    let pokemonName = answers.pokemonName;
-    fetchPokemon(pokemonName);
-    console.log(pokemonName);
-  });
-};
+    const pokemonJSON = await fetchPokemon(pokemonName.pokemonName);
+    console.log(pokemonJSON.name, pokemonJSON.weight);
 
-const promptForDownloadInfo = async () => {
-  inquirer
-    .prompt([
-      {
-        type: "checkbox",
-        message: "Select info to download!",
-        name: "downloadInfos",
-        choices: [
-          new inquirer.Separator(" -- Options -- "),
-          {
-            name: "Stats",
-          },
-          {
-            name: "Sprites",
-          },
-          {
-            name: "Artwork",
-          },
-        ],
-        validate(answer) {
-          if (answer.length < 1) {
-            return "You must choose at least one info to download.";
-          }
+    const pokemonOptions = await promptForDownloadInfo();
+    console.log(pokemonOptions.options);
 
-          return true;
-        },
-      },
-    ])
-    .then((answers) => {
-      console.log(answers.downloadInfos);
-    });
+    const keepGoing = await promptToContinue();
+    if (keepGoing.continueAnotherDownload === false) {
+      break;
+    }
+  }
 };
