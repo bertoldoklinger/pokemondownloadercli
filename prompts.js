4407d930374a62b4760e543a1d4d7ef7a7ececb6
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-01-10 15:26:01 -0300

    added create folder,saving stats,saving artwork logic

--- prompts.js
+++ prompts.js
@@ -62,3 +62,5 @@ const promptUser = async () => {
     }
   }
 };
+
+export { fetchPokemon };
--- saving.js
+++ saving.js
@@ -1,15 +1,42 @@
-import fetch from "node-fetch";
 import fs from "fs/promises";
 import path from "path";
+import { fetchPokemon } from "./prompts.js";
+import fetch from "node-fetch";
 
-// cria uma pasta
+const saveImageFile = async (filePath, arrayBuffer) => {
+  await fs.writeFile(filePath, Buffer.from(arrayBuffer));
+};
 
-// salva o arquivo de imagem
+const createFolder = async (folderName) => {
+  const folderPath = path.join(process.cwd(), folderName);
+  try {
+    await fs.access(folderPath);
+  } catch {
+    fs.mkdir(folderPath);
+  }
+};
 
-//salva os status do pokemon escolhido em png
+const pokemonObject = await fetchPokemon("mewtwo");
 
-// salva os sprites do pokemon escolhido
+const savePokemonStats = async (folderName, pokemonStatsObject) => {
+  let statsString = "";
+  for (const stat of pokemonStatsObject) {
+    statsString += `${stat.stat.name} : ${stat.base_stat}\n`;
+  }
 
-//salva artwork do pokemon escolhido
+  await createFolder(folderName);
 
-//converte as opçoes(objeto para o modelo escolhido)
+  const filePath = path.join(process.cwd(), folderName, "stats.txt");
+  await fs.writeFile(filePath, statsString);
+};
+
+const savePokemonArtwork = async (folderName, pokemonSpritesObject) => {
+  const url = pokemonSpritesObject.other["official-artwork"].front_default;
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  await fs.writeFile("artwork.png", Buffer.from(arrayBuffer));
+  const filePath = path.join(process.cwd(), folderName, "artwork.png");
+  await saveImageFile(filePath, arrayBuffer);
+};
+
+savePokemonArtwork("mewtwo", pokemonObject.sprites);
